bd581e046c3dbfa77059f516ee5f80f1b608a4f1
Kyuwoong Lee <kwooler@gmail.com>
2023-10-04 22:29:54 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -6,7 +6,7 @@ import random
 def test_main_1():
 
     numbers = [-10, 20, 30, 40, -50]
-    maxval, minval = main.minmax(numbers)
+    minval, maxval = main.minmax(numbers)
 
     assert maxval == 40, "Max value does not match"
     assert minval == -50, "Min value does not match"
@@ -17,7 +17,7 @@ def test_main_2():
     numbers = [random.randint(0, 100) for i in range(10)]
     miv = min(numbers)
     mav = max(numbers)
-    maxval, minval = main.minmax(numbers)
+    minval, maxval = main.minmax(numbers)
 
     assert maxval == mav, "Min value does not match"
     assert minval == miv, "Max value does not match"
