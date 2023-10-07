55b69ad035239b5334bf8936674f3a411767d36a
Kyu Lee <kwooler@gmail.com>
2023-10-07 13:39:19 -0700

    Q5Q4

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def minmax(numbers):
 def main():
 
     numbers = [1, 2, 3, 4, 5]
-    maxval, minval = minmax(numbers)
+    minval, maxval = minmax(numbers)
     print('List values', *numbers)
     print(f'Max value: {maxval} \t Min value:{minval}')
 
     numbers = [random.randint(0, 100) for i in range(10)]
-    maxval, minval = minmax(numbers)
+    minval, maxval = minmax(numbers)
     print('List values', *numbers)
     print(f'Max value: {maxval} \t Min value: {minval}')
 
